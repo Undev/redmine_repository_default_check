bfe44192926bbd3e03505f3efe0cb1dcfb9197da
Danil Tashkinov <danil.tashkinov@gmail.com>
2015-04-28 10:06:30 +0300

    Bump version to 1.0.0

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_repository_default_check do
   name 'Redmine Default Repository Check Plugin'
   author 'Undev'
   description 'This plugin checks if the main repository is set for a Redmine project and other repositories have identifiers.'
-  version '0.1.1'
+  version '1.0.0'
   url 'https://github.com/Undev/redmine_repository_default_check'
   author_url 'https://github.com/Undev'
   requires_redmine :version_or_higher => '2.0.0'
